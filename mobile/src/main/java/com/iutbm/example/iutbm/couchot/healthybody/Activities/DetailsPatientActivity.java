d85d7607ecba2d88aaf3247a6651f4280ea616c4
rkessal <kessal.rayhan25@gmail.com>
2020-01-28 15:53:41 +0100

    fragment + activity details patient

---
+++ mobile/src/main/java/com/iutbm/example/iutbm/couchot/healthybody/Activities/DetailsPatientActivity.java
@@ -0,0 +1,16 @@
+package com.iutbm.example.iutbm.couchot.healthybody.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.iutbm.example.iutbm.couchot.healthybody.R;
+
+public class DetailsPatientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_patient);
+    }
+}
